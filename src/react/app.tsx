59ec1cb054477aabfa0fd225f719f9eacaee0095
RashingPro <rashingpro@yandex.ru>
2025-07-28 17:49:13 +0300

    refactor: reorganize code in app.tsx

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,14 +1,34 @@
 import { createRoot } from "react-dom/client";
-import CopyIcon from "@rsuite/icons/Copy";
-import CloseIcon from "@rsuite/icons/Close";
-import CollaspedOutlineIcon from "@rsuite/icons/CollaspedOutline";
 import { useEffect, useRef, useState } from "react";
 import "./app.css";
+import { IconCopy, IconMinus, IconSquareX } from "@tabler/icons-react";
 
 const root = createRoot(document.body);
 root.render(<RootComponent />);
 
-function TitleBar() {
+function TitleBar(props: { windowTitle: string; height: number }) {
+    return (
+        <div className={"titlebar"} style={{ height: `${props.height}px`, minHeight: `${props.height}px` }}>
+            <div style={{ justifyContent: "left" }} className={"app-drag"}>
+                <span>{props.windowTitle}</span>
+            </div>
+            <div style={{ justifyContent: "right" }} className={"app-no-drag"}>
+                <button className={"titlebar__minimize"} onClick={window.electronApi.window.toggleMinimize}>
+                    {/*<CollaspedOutlineIcon />*/}
+                    <IconMinus />
+                </button>
+                <button className={"titlebar__maximize"} onClick={window.electronApi.window.toggleMaximize}>
+                    {/*<CopyIcon />*/}
+                    <IconCopy transform={"scale(-1, 1)"} />
+                </button>
+                <button className={"titlebar__close"} onClick={window.electronApi.window.closeWindow}>
+                    {/*<CloseIcon />*/}
+                    <IconSquareX />
+                </button>
+            </div>
+        </div>
+    );
+function RootComponent() {
     const [isLoaded, setIsLoaded] = useState(false);
     const titleBarHeight = useRef<number>(null);
     const windowTitle = useRef<string>(null);
@@ -17,40 +37,17 @@ function TitleBar() {
         (async () => {
             titleBarHeight.current = await window.electronApi.constants.getTitleBarHeight();
             windowTitle.current = await window.electronApi.constants.getWindowTitle();
-            console.log(`Got height: ${titleBarHeight.current}`);
-            console.log(`Got window title: ${windowTitle.current}`);
             setIsLoaded(true);
         })();
     }, []);
 
     if (isLoaded) {
         window.electronApi.events.rootLoaded();
-
         return (
-            <div className={"titlebar"} style={{ height: `${titleBarHeight.current}px` }}>
-                <div style={{ justifyContent: "left" }} className={"app-drag"}>
-                    {windowTitle.current}
-                </div>
-                <div style={{ justifyContent: "right" }} className={"app-no-drag"}>
-                    <button className={"titlebar__minimize"} onClick={window.electronApi.window.toggleMinimize}>
-                        <CollaspedOutlineIcon />
-                    </button>
-                    <button className={"titlebar__maximize"} onClick={window.electronApi.window.toggleMaximize}>
-                        <CopyIcon />
-                    </button>
-                    <button className={"titlebar__close"} onClick={window.electronApi.window.closeWindow}>
-                        <CloseIcon />
-                    </button>
-                </div>
+            <div className={"root"}>
+                <TitleBar windowTitle={windowTitle.current} height={titleBarHeight.current} />
+                <div className={"page-content"}></div>
             </div>
         );
     }
-}
-
-function RootComponent() {
-    return (
-        <>
-            <TitleBar />
-        </>
-    );
 }
